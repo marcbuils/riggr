759e90127e01b73a8b48793617c65c0203c42a27
jdubya <woodenconsulting@gmail.com>
2014-08-04 12:30:02 -0400

    grunted

--- src/date.js
+++ src/date.js
@@ -108,7 +108,7 @@
         'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
       ]
     },
-    
+
     /**
      * Adds leading zeros to a string
      * @param val {String} The value to pad
