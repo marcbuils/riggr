e1375434d10d82411e3a0f43b1760d497a5c5cb5
fluidbyte <kent@fluidbyte.net>
2014-09-30 14:08:05 -0400

    Fixing race conditions with lib loading

--- src/riggr.js
+++ src/riggr.js
@@ -45,20 +45,33 @@
   };
 
   // Apply libs
-  var applyLibs = function (controller) {
+  var applyLibs = function (controller, cb) {
+    var totalLibs;
+    var libsLoaded = 0;
     // Applies lib to controller lib object
     var applyLib = function (lib) {
       require([paths.libs + '/' + controller.libs[lib]], function (cur) {
         controller.libs[lib] = cur;
+        // Increment libs loaded count
+        libsLoaded++;
+        // All libs loaded?
+        if (libsLoaded === totalLibs) {
+          // Fire callback
+          cb();
+        }
       });
     };
 
     // Add libs to controller
     if (controller.hasOwnProperty('libs')) {
+      totalLibs = Object.keys(controller.libs).length;
       for (var lib in controller.libs) {
         // Set libs.{key} to required lib for use
         applyLib(lib);
       }
+    } else {
+      // No libs, fire callback
+      cb();
     }
   };
 
@@ -136,18 +149,20 @@
       }
 
       // Apply libs
-      applyLibs(controller);
+      applyLibs(controller, function () {
 
-      // Create route
-      router.on(route, routeHandler);
+        // Create route
+        router.on(route, routeHandler);
 
-      // Increment loaded tracker
-      loaded++;
+        // Increment loaded tracker
+        loaded++;
 
-      // On last route, process...
-      if (count === loaded) {
-        loadApp();
-      }
+        // On last route, process...
+        if (count === loaded) {
+          loadApp();
+        }
+
+      });
 
     });
   };
@@ -199,27 +214,27 @@
     }
 
     // Apply libs
-    applyLibs(app);
+    applyLibs(app, function () {
+      // Set title
+      appTitle = (app.hasOwnProperty('title')) ? app.title : false;
+      setTitle('Loading');
 
-    // Set title
-    appTitle = (app.hasOwnProperty('title')) ? app.title : false;
-    setTitle('Loading');
+      // Set transition
+      transition = (app.hasOwnProperty('transition')) ? app.transition : 0;
 
-    // Set transition
-    transition = (app.hasOwnProperty('transition')) ? app.transition : 0;
+      // Set count
+      count = Object.size(app.routes);
 
-    // Set count
-    count = Object.size(app.routes);
+      // Fire the app controller's init method
+      if (app.init && {}.toString.call(app.init) === '[object Function]') {
+        app.init();
+      }
 
-    // Fire the app controller's init method
-    if (app.init && {}.toString.call(app.init) === '[object Function]') {
-      app.init();
-    }
-
-    // Build controller+route handlers
-    for (var route in app.routes) {
-      build(route, app.routes[route]);
-    }
+      // Build controller+route handlers
+      for (var route in app.routes) {
+        build(route, app.routes[route]);
+      }
+    });
   };
 
   return rigg;
